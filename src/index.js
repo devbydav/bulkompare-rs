1eb815d63df3e7158c3c0fe547ec7b61f2b83e42
David <devbydav@gmail.com>
2022-02-25 11:59:44 +0100

    Add custom theme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router,} from "react-router-dom";
+import {createTheme, ThemeProvider} from "@mui/material";
+import {green} from "@mui/material/colors";
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: green[800],
+        },
+    },
+});
 
 ReactDOM.render(
     // <React.StrictMode>
     <Router>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </Router>,
     // </React.StrictMode>,
     document.getElementById('root')

d6cfc2e993304521bf17014a86b6217a00008947
David <devbydav@gmail.com>
2022-02-25 12:51:34 +0100

    Show open/save selection in config drawer only

--- src/components/NavBar/ConfigControl.js
+++ src/components/NavBar/ConfigControl.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Button, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
+import {Button, Divider, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 
 import {Status} from "../../constants/constants";
 import Steps from "./Steps";
 import SuperSelect from "./SuperSelect";
 import {useNavigate} from "react-router-dom";
+import {open, save} from "@tauri-apps/api/dialog";
+import {invoke} from "@tauri-apps/api/tauri";
+import FolderOpenOutlinedIcon from "@mui/icons-material/FolderOpenOutlined";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 
 
-const ConfigControl = ({selection, selectedExt, setSelectedExt, comparator, handleCompare}) => {
+const ConfigControl = ({selection, setSelection, selectedExt, setSelectedExt, comparator, handleCompare, showToast}) => {
     console.log("-> Rendering ConfigControl with", selectedExt);
 
     const navigate = useNavigate();
@@ -23,9 +27,72 @@ const ConfigControl = ({selection, selectedExt, setSelectedExt, comparator, hand
 
     if (!comparator) return(<></>);
 
+    const handleSaveSelection = () => {
+        console.log("saving sel", selection);
+
+        save({
+            defaultPath: "config/default.json",
+            filters: [
+                {
+                    name: "Fichier sélection",
+                    extensions: ["json"],
+                },
+            ],
+        })
+            .then(path => {
+                if (path) {
+                    return invoke('save_selection', {path, selection: selection})
+                        .then(() => showToast("Sélection enregistrée"));
+                }
+            })
+            .catch(e => showToast(e, false))
+    };
+
+    const handleOpenSelection = () => {
+        console.log("opening sel", selection);
+
+        open({
+            defaultPath: "config",
+            filters: [
+                {
+                    name: "Fichier sélection",
+                    extensions: ["json"],
+                },
+            ],
+            multiple: false,
+            directory: false,
+        })
+            .then(path => {
+                if (path) {
+                    return invoke('open_selection', {path})
+                        .then((newSelection) => {
+                            setSelection(newSelection);
+                            showToast("Nouvelle sélection importée")
+                        })
+                }
+            })
+            .catch(e => showToast(e, false));
+    };
+
 
     return (
         <>
+            <List>
+                <ListItem button onClick={handleOpenSelection}>
+                    <ListItemIcon>
+                        <FolderOpenOutlinedIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="Ouvrir sélection"/>
+                </ListItem>
+
+                <ListItem button onClick={handleSaveSelection}>
+                    <ListItemIcon>
+                        <SaveOutlinedIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="Sauver sélection"/>
+                </ListItem>
+            </List>
+            <Divider/>
             <List>
                 <ListItem button onClick={() => navigate('/source')}>
                     <ListItemIcon>
--- src/components/NavBar/LeftDrawer.js
+++ src/components/NavBar/LeftDrawer.js
@@ -1,19 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {invoke} from '@tauri-apps/api/tauri';
-import {open, save} from '@tauri-apps/api/dialog';
 import {useLocation} from 'react-router-dom';
 import {
     Button,
     ButtonGroup,
     Divider,
     Drawer,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
 } from '@mui/material';
-import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
-import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+
 import ConfigControl from "./ConfigControl";
 import ResultControl from "./ResultControl";
 import {drawerWidth} from "../../constants/constants"
@@ -69,52 +62,7 @@ const LeftDrawer = ({selection, setSelection, comparator, selectedExt, setSelect
 
     console.log('location', location);
 
-    const handleSaveSelection = () => {
-        console.log("saving sel", selection);
 
-        save({
-            defaultPath: "config/default.json",
-            filters: [
-                    {
-                        name: "Fichier sélection",
-                        extensions: ["json"],
-                    },
-                ],
-        })
-            .then(path => {
-                if (path) {
-                    return invoke('save_selection', {path, selection: selection})
-                        .then(() => showToast("Sélection enregistrée"));
-                }
-            })
-            .catch(e => showToast(e, false))
-    };
-
-    const handleOpenSelection = () => {
-        console.log("opening sel", selection);
-
-        open({
-            defaultPath: "config",
-            filters: [
-                {
-                    name: "Fichier sélection",
-                    extensions: ["json"],
-                },
-            ],
-            multiple: false,
-            directory: false,
-        })
-            .then(path => {
-                if (path) {
-                    return invoke('open_selection', {path})
-                        .then((newSelection) => {
-                            setSelection(newSelection);
-                            showToast("Nouvelle sélection importée")
-                        })
-                }
-            })
-            .catch(e => showToast(e, false));
-    };
 
     return (
         <Drawer
@@ -146,30 +94,16 @@ const LeftDrawer = ({selection, setSelection, comparator, selectedExt, setSelect
 
             </ButtonGroup>
             <Divider/>
-            <List>
-                <ListItem button onClick={handleOpenSelection}>
-                    <ListItemIcon>
-                        <FolderOpenOutlinedIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary="Ouvrir sélection"/>
-                </ListItem>
 
-                <ListItem button onClick={handleSaveSelection}>
-                    <ListItemIcon>
-                        <SaveOutlinedIcon/>
-                    </ListItemIcon>
-                    <ListItemText primary="Sauver sélection"/>
-                </ListItem>
-            </List>
-            <Divider/>
             {configuring ?
-
                 <ConfigControl
                     selection={selection}
+                    setSelection={setSelection}
                     selectedExt={selectedExt}
                     setSelectedExt={setSelectedExt}
                     comparator={comparator}
                     handleCompare={handleCompare}
+                    showToast={showToast}
                 />
                 :
                 <ResultControl
